f8bc65851629e010dab9ffa5bd9d20fc67374fc1
Kirill <dolgorukov-22122003@mail.ru>
2024-10-05 21:38:48 +0300

    update forum.sql

--- forum.sql
+++ forum.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 05 2024 г., 19:24
+-- Время создания: Окт 05 2024 г., 21:37
 -- Версия сервера: 8.0.30
--- Версия PHP: 8.1.9
+-- Версия PHP: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -81,7 +81,7 @@ INSERT INTO `message` (`id`, `text`, `created_at`, `id_user`, `id_thread`) VALUE
 
 CREATE TABLE `message_image` (
   `id` int NOT NULL,
-  `path` text NOT NULL,
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `id_message` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -150,7 +150,7 @@ INSERT INTO `thread` (`id`, `title`, `text`, `created_at`, `closed`, `id_user`, 
 CREATE TABLE `thread_image` (
   `id` int NOT NULL,
   `id_thread` int NOT NULL,
-  `path` text NOT NULL
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
