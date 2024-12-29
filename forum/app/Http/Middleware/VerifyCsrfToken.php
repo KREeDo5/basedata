b3c6b7f9f6242d18b393e68570eb474207496cf0
dmitrii.liashko <dmitrii.liashko>
2024-12-29 18:34:47 +0300

    Revert "fix 'success'"
    This reverts commit c6928ed1d2e85c8b5ff35f4bf2864b2c88623ebc.

--- forum/app/Http/Middleware/VerifyCsrfToken.php
+++ forum/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,9 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'registration/check',
-        'auth/check',
-        'change/profile',
-        'change/profile/image'
+        //
     ];
 }
