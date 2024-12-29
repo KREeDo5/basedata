3cc0c32109e885bf5489959872ef7cffa6ac864a
dmitrii.liashko <dmitrii.liashko>
2024-12-29 18:35:02 +0300

    Revert "fix"
    This reverts commit a407b20cb3d37522e4f57b700b2956ed5d975e3c.

--- forum/app/Console/Kernel.php
+++ forum/app/Console/Kernel.php
@@ -38,9 +38,4 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
-    
-    protected $middleware = [
-        // Другие middleware
-        \Fruitcake\Cors\HandleCors::class,
-    ];
 }
