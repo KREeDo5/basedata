8485abdccfd211330039919abb42b596a7a69906
dmitrii.liashko <dmitrii.liashko>
2024-12-30 04:36:16 +0300

    Добавил форму поиска тем

--- forum/src/pages/Home.vue
+++ forum/src/pages/Home.vue
@@ -21,20 +21,30 @@ const sortOptions = [
 </script>
 
 <template>
-  <HeaderBar/>
+  <HeaderBar />
   <div class="max-w-[1520px] w-full mx-auto mt-10 py-5 flex">
     <CategoryBlock :categoryList="categories" />
     <div class="w-full">
-      <div class="bg-base-darkgrey rounded-[20px] py-3 px-4 mb-5">
-        <form>
+      <div class="flex bg-base-darkgrey rounded-[20px] py-3 px-4 mb-5">
+        <form class="mr-3">
           <select
             id="sortType"
-            class="w-[260px] bg-base-grey text-base-grey2 border border-base-grey text-base rounded-[12px] focus:border-blue-500 block p-2.5"
+            class="w-[260px] p-2.5 block bg-base-grey text-base-grey2 border border-base-grey text-base rounded-[12px] focus:border-blue-500"
           >
             <option v-for="option in sortOptions" :key="option.value" :value="option.value">
               {{ option.text }}
             </option>
           </select>
+        </form>
+        <form class="flex-1 relative">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <img src="/search.png" alt="search-icon" />
+          </div>
+          <input
+            type="text"
+            placeholder="Поиск тем"
+            class="w-full p-2.5 ps-10 block bg-base-grey text-base-grey2 text-base rounded-[12px] focus:outline-none focus:ring-1 focus:ring-base-blue"
+          />
         </form>
       </div>
       <ThreadsBlock :threadList="threads" />
