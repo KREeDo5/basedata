877595a7e26f09154d3d4ba86459a9937cab1eae
dmitrii.liashko <dmitrii.liashko>
2025-01-11 20:37:24 +0300

    Авторизация/Регистрация профиля - обработка id

--- forum/src/stores/AuthStore.js
+++ forum/src/stores/AuthStore.js
@@ -38,8 +38,9 @@ export const useAuthStore = defineStore('authStore', () => {
       }
       const response = await axios.post('https://forum.kreedo.tech:8443/registration', form)
       const meta = response.data.meta
+      const data = response.data.data
       if (meta.success) {
-        const newToken = '9' // TODO: обработка токена
+        const newToken = data.userId
         localStorage.setItem('auth_token', newToken)
         token.value = newToken // Обновляем реактивное значение
       } else {
@@ -56,7 +57,7 @@ export const useAuthStore = defineStore('authStore', () => {
       const meta = response.data.meta
       const data = response.data.data
       if (meta.success) {
-        const newToken = data.id
+        const newToken = data.userId
         localStorage.setItem('auth_token', newToken)
         token.value = newToken // Обновляем реактивное значение
       } else {
