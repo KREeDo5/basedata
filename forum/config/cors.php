0d16c948e3f02cfc1d5e1ebc224609dfb286ac14
Kirill <dolgorukov-22122003@mail.ru>
2025-01-13 01:02:24 +0300

    Update cors.php

--- forum/config/cors.php
+++ forum/config/cors.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'registration/check'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'registration/check', 'create/message'],
 
     'allowed_methods' => ['*'],
 
